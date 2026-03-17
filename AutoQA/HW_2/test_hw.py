ec0f577cf5b95ea2135663af1b62909b8b61a246
Nina <ninkothir@users.noreply.github.com>
2026-03-17 12:34:04 +0100

    Fix HW_2 with fixture

--- AutoQA/HW_2/test_hw.py
+++ AutoQA/HW_2/test_hw.py
@@ -1,23 +1,23 @@
+import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 
-def test_payment_section():
+@pytest.fixture
+def driver():
     driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
 
-    try:
-        driver.get("https://itcareerhub.de/ru")
-        driver.maximize_window()
-        time.sleep(3)
 
-        # Переходим в раздел "Способы оплаты"
-        payment_link = driver.find_element(By.LINK_TEXT, "Способы оплаты")
-        payment_link.click()
-        time.sleep(3)
+def test_payment_section(driver):
+    driver.get("https://itcareerhub.de/ru/")
+    driver.maximize_window()
+    time.sleep(3)
 
-        # Делаем скриншот
-        driver.save_screenshot("payment_section.png")
+    payment_link = driver.find_element(By.LINK_TEXT, "Способы оплаты")
+    payment_link.click()
+    time.sleep(3)
 
-    finally:
-        driver.quit()
+    driver.save_screenshot("payment_section.png")
