8fe99a17c63c31e4daa4ccd182bf84918040112c
Nina <ninkothir@users.noreply.github.com>
2026-03-19 11:13:30 +0100

    fix HW_5 and project structure

--- AutoQA/HW_5/test_iframe.py
+++ AutoQA/HW_5/test_iframe.py
@@ -1,26 +1,19 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_iframe_text():
-
-    driver = webdriver.Chrome()
+def test_iframe_text(driver):
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/iframes.html")
 
     wait = WebDriverWait(driver, 10)
 
-    iframe = wait.until(
-        EC.presence_of_element_located((By.TAG_NAME, "iframe"))
+    wait.until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME, "iframe")))
+
+    paragraphs = wait.until(
+        EC.presence_of_all_elements_located((By.TAG_NAME, "p"))
     )
 
-    driver.switch_to.frame(iframe)
+    target_text = "semper posuere integer et senectus justo curabitur."
 
-    text_element = wait.until(
-        EC.presence_of_element_located((By.XPATH, "//p[2]"))
-    )
-
-    assert "semper posuere integer et senectus justo curabitur." in text_element.text
-
-    driver.quit()
+    assert any(target_text in p.text for p in paragraphs)
